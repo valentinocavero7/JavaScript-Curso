4644b2c7a8409acbf561200b000139cc2cf445bf
Mario <111661567+valentinocavero7@users.noreply.github.com>
2023-08-26 12:27:53 -0500

    terminar ejercicio 9-I

--- sesion_09/9i.js
+++ sesion_09/9i.js
@@ -1,10 +1,10 @@
 let inicio = 0;
 function actualizarCarta(cart) {
-   if(inicio += cart > 12) {
-    console.log('No hay más cartas por remover');
-    return;
-   } else if(inicio += cart < 0) {
+   if(inicio + cart > 12) {
     console.log('No se puede agregar más cartas');
+    return;
+   } else if(inicio + cart < 0) {
+    console.log('No se puede remover más cartas');
     return;
    }
 
